6addf443e2df718cc8f36b0fe03b4d04308bf4bc
xsandrk <99973233+xsandrk@users.noreply.github.com>
2025-05-02 17:06:54 +0400

    Add samba

---
+++ install/samba-install.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2025
+# Author: xsandrk
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# Source:
+
+source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_info "Installing Samba"
+$STD apt-get install -y samba
+msg_info "Installed Samba"
+
+motd_ssh
+customize
+
+msg_info "Cleaning up"
+$STD apt-get -y autoremove
+$STD apt-get -y autoclean
+msg_ok "Cleaned"
