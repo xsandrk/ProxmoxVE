15434e6e6f446ee50812696d3c7306dab7372c81
xsandrk <99973233+xsandrk@users.noreply.github.com>
2025-05-07 16:46:24 +0400

    Add 3proxy cfg

--- install/3proxy-install.sh
+++ install/3proxy-install.sh
@@ -18,6 +18,30 @@ $STD wget https://github.com/3proxy/3proxy/releases/download/0.9.5/3proxy-0.9.5.
 $STD apt install -y /tmp/3proxy-0.9.5.x86_64.deb
 msg_info "Installed 3proxy"
 
+msg_info "Configure 3proxy"
+cat <<EOF >/etc/3proxy/conf/3proxy.cfg
+nserver 8.8.8.8
+nserver 8.8.4.4
+nscache 65536
+
+#log /logs/3proxy-%y%m%d.log D
+#rotate 5
+
+auth iponly
+allow * * $/conf/domains.lst
+parent 1000 socks5+ 127.0.0.1 1080
+allow *
+proxy
+EOF
+
+cat <<EOF >/etc/3proxy/conf/domains.lst
+*themoviedb.org,*lostfilm.tv,*nnmclub.to,*youtube.com
+EOF
+
+systemctl start 3proxy
+
+msg_info "Configured 3proxy"
+
 motd_ssh
 customize
 
